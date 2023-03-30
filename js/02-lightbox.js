36580f1cd69c25dc09ef1cd4adef0be11d0617f3
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-03-30 22:12:02 +0200

    Task 2 update SimpleLightbox (option) and Finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,9 +5,12 @@ const galleryEl = document.querySelector(".gallery");
 const images = galleryItems
   .map(
     (photo) =>
-      `<li class="gallery__item"><a class="gallery__link" href="${photo.original}"><img class="gallery__image" src="${photo.preview}"  alt="" title="${photo.description}" /></a></li>`
+      `<li class="gallery__item"><a class="gallery__link" href="${photo.original}"><img class="gallery__image" src="${photo.preview}"  alt="${photo.description}"  /></a></li>`
   )
   .join("");
 galleryEl.insertAdjacentHTML("beforeend", images);
 
-let lightbox = new SimpleLightbox(".gallery a", {});
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
